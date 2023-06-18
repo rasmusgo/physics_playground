7293c353a489552b83bcf9f83b78a6d5aa574061
Rasmus Brönnegård <1162652+rasmusgo@users.noreply.github.com>
2023-06-18 14:47:40 +0200

    WIP geometric algebra

--- src/ga_g3.rs
+++ src/ga_g3.rs
@@ -1,26 +1,44 @@
 use std::ops::{Add, Mul};
 
 use g3;
+use itertools::Itertools;
 use nalgebra::{Matrix3, Vector3};
 use rand::{rngs::StdRng, seq::SliceRandom, Rng, SeedableRng};
-use rerun::{
-    components::{ColorRGBA, Point3D, Radius, ViewCoordinates},
-    coordinates::{Handedness, SignedAxis3},
-    demo_util::lerp,
-    external::glam::{self, vec3, Vec3},
-    time::{Time, TimeType, Timeline},
-    MsgSender, RecordingStreamBuilder,
-};
+mod rr {
+    pub use rerun::{
+        components::{Arrow3D, ColorRGBA, Point3D, Radius, Vec3D, ViewCoordinates},
+        coordinates::{Handedness, SignedAxis3},
+        demo_util::lerp,
+        external::glam::{self, vec3, Vec3},
+        time::{Time, TimeType, Timeline},
+        MsgSender, RecordingStreamBuilder,
+    };
+}
+
+trait IntoRerun<T> {
+    type Output;
+    fn into_rerun(self) -> Self::Output;
+}
+
+impl IntoRerun<g3::Point> for g3::Point {
+    type Output = rr::Point3D;
+    fn into_rerun(self) -> Self::Output {
+        unsafe { rr::Point3D::new(self.x(), self.y(), self.z()) }
+    }
+}
 
 fn is_colliding(plane: &g3::Plane, points: &[g3::Point]) -> bool {
     // TODO: Should this really test only the scalar part?
     points.iter().any(|p| (*p & *plane).scalar() < 0.0)
 }
 
-fn is_edge(i: i32, j: i32) -> bool {
+fn is_edge(i: usize, j: usize) -> bool {
     let x = i ^ j; // xor both together, all differing bits will be '1'
     (x & (x - 1)) == 0 // only if x has one bit, this returns true.
 }
+
+const ATTACH_ANCHOR: g3::Point = g3::point(0.0, 0.0, 1.5);
+const ATTACH_IN_BODY: g3::Point = g3::point(0.5, 0.5, 0.5);
 
 struct State {
     pub m: g3::Motor,
@@ -28,31 +46,37 @@ struct State {
 }
 
 struct DState {
-    pub d_motor: g3::Motor,
-    pub d_branch: g3::Line,
+    pub d_m: g3::Motor,
+    pub d_b: g3::Line,
+}
+
+struct Edge {
+    pub i: usize,
+    pub j: usize,
 }
 
 fn dState(state: &State) -> DState {
     DState {
-        d_motor: g3::Scalar::new(-0.5).geometric_product(state.m.geometric_product(state.b)),
-        d_branch: g3::Scalar::new(-0.5)
-            .geometric_product((state.b.dual() * state.b - state.b * state.b.dual()).dual()),
+        d_m: -0.5 * state.m * state.b,
+        d_b: -0.5 * ((state.b.dual() * state.b - state.b * state.b.dual()).dual()),
     }
 }
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let recording =
-        RecordingStreamBuilder::new("GA physics").connect(rerun::default_server_addr())?;
-    let stable_time = Timeline::new("stable_time", TimeType::Time);
-    MsgSender::new("world")
+        rr::RecordingStreamBuilder::new("GA physics g3").connect(rerun::default_server_addr())?;
+    let stable_time = rr::Timeline::new("stable_time", rr::TimeType::Time);
+    rr::MsgSender::new("world")
         .with_timeless(true)
-        .with_splat(ViewCoordinates::from_up_and_handedness(
-            SignedAxis3::POSITIVE_Z,
-            Handedness::Right,
+        .with_splat(rr::ViewCoordinates::from_up_and_handedness(
+            rr::SignedAxis3::POSITIVE_Z,
+            rr::Handedness::Right,
         ))?
         .send(&recording)?;
 
-    let p = [
+    let radius = rr::Radius(0.025);
+
+    let points_in_local = [
         g3::point(-0.5, -0.5, -0.5),
         g3::point(-0.5, -0.5, 0.5),
         g3::point(-0.5, 0.5, -0.5),
@@ -63,15 +87,59 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         g3::point(0.5, 0.5, 0.5),
     ];
 
+    let edges: Vec<Edge> = (0..points_in_local.len())
+        .combinations(2)
+        .filter_map(|v| {
+            if is_edge(v[0], v[1]) {
+                Some(Edge { i: v[0], j: v[1] })
+            } else {
+                None
+            }
+        })
+        .collect();
+
     let mut state = State {
-        motor: g3::Motor::default(),
-        b: g3::Line::default(),
+        m: g3::Motor::one(),
+        b: g3::Line::new(0.0, 0.0, 0.0, 1.0, 1.3, 0.5),
     };
     let dt = 0.001;
     for i in 0..3000 {
         let d_state = dState(&state);
-        state.motor += dt * d_state.d_motor;
-        state.b += dt * d_state.d_branch;
+        state.m += dt * d_state.d_m;
+        state.b += dt * d_state.d_b;
+
+        let points = points_in_local
+            .iter()
+            .map(|p_in_local| state.m.transformation(*p_in_local).into_rerun())
+            .collect::<Vec<rr::Point3D>>();
+        let lines = edges
+            .iter()
+            .map(|e| rr::Arrow3D {
+                origin: [points[e.i].x, points[e.i].y, points[e.i].z].into(),
+                vector: [
+                    points[e.j].x - points[e.i].x,
+                    points[e.j].y - points[e.i].y,
+                    points[e.j].z - points[e.i].z,
+                ]
+                .into(),
+            })
+            .collect::<Vec<rr::Arrow3D>>();
+
+        rr::MsgSender::new("world/points")
+            .with_time(
+                stable_time,
+                rr::Time::from_seconds_since_epoch(i as f64 * dt as f64),
+            )
+            .with_component(&points)?
+            .with_splat(radius)?
+            .send(&recording)?;
+        rr::MsgSender::new("world/lines")
+            .with_time(
+                stable_time,
+                rr::Time::from_seconds_since_epoch(i as f64 * dt as f64),
+            )
+            .with_component(&lines)?
+            .send(&recording)?;
     }
 
     Ok(())
--- src/ga_ganja.rs
+++ src/ga_ganja.rs
@@ -1,9 +1,10 @@
 use std::ops::{Add, Mul};
 
-use geometric_algebra::*;
 use itertools::Itertools;
 use nalgebra::{Matrix3, Vector3};
 use rand::{rngs::StdRng, seq::SliceRandom, Rng, SeedableRng};
+mod pga3d;
+use pga3d::{Basis, PGA3D};
 mod rr {
     pub use rerun::{
         components::{Arrow3D, ColorRGBA, Point3D, Radius, Vec3D, ViewCoordinates},
@@ -20,24 +21,16 @@ trait IntoRerun<T> {
     fn into_rerun(self) -> Self::Output;
 }
 
-impl IntoRerun<ppga3d::Point> for ppga3d::Point {
+impl IntoRerun<PGA3D> for PGA3D {
     type Output = rr::Point3D;
     fn into_rerun(self) -> Self::Output {
-        unsafe {
-            rr::Point3D::new(
-                self.group0().f32x4[1],
-                self.group0().f32x4[2],
-                self.group0().f32x4[3],
-            )
-        }
+        rr::Point3D::new(self[11] as _, self[12] as _, self[13] as _)
     }
 }
 
-fn is_colliding(plane: &ppga3d::Plane, points: &[ppga3d::Point]) -> bool {
+fn is_colliding(plane: &PGA3D, points: &[PGA3D]) -> bool {
     // TODO: Should this really test only the scalar part?
-    points
-        .iter()
-        .any(|p| (p.regressive_product(*plane)).group0() < 0.0)
+    points.iter().any(|p| (p & plane)[0] < 0.0)
 }
 
 fn is_edge(i: usize, j: usize) -> bool {
@@ -45,17 +38,17 @@ fn is_edge(i: usize, j: usize) -> bool {
     (x & (x - 1)) == 0 // only if x has one bit, this returns true.
 }
 
-const ATTACH_ANCHOR: ppga3d::Point = ppga3d::Point::new(1.0, 0.0, 0.0, 1.5);
-const ATTACH_IN_BODY: ppga3d::Point = ppga3d::Point::new(1.0, 0.5, 0.5, 0.5);
+const ATTACH_ANCHOR: PGA3D = PGA3D::point(0.0, 0.0, 1.5);
+const ATTACH_IN_BODY: PGA3D = PGA3D::point(0.5, 0.5, 0.5);
 
 struct State {
-    pub world_from_local: ppga3d::Motor,
-    pub velocity_in_local: ppga3d::Line,
+    pub world_from_local: PGA3D,
+    pub velocity_in_local: PGA3D,
 }
 
 struct DState {
-    pub d_world_from_local: ppga3d::Motor,
-    pub d_velocity_in_local: ppga3d::Line,
+    pub d_world_from_local: PGA3D,
+    pub d_velocity_in_local: PGA3D,
 }
 
 struct Edge {
@@ -64,43 +57,51 @@ struct Edge {
 }
 
 fn dState(state: &State) -> DState {
+    let world_from_local = &state.world_from_local;
+    let velocity_in_local = &state.velocity_in_local;
+    let d_velocity_in_local = PGA3D::from_elements(
+        0.0,
+        0.0,
+        0.0,
+        0.0,
+        0.0,
+        velocity_in_local[Basis::e02 as _] * velocity_in_local[Basis::e12 as _]
+            - velocity_in_local[Basis::e03 as _] * velocity_in_local[Basis::e31 as _],
+        velocity_in_local[Basis::e03 as _] * velocity_in_local[Basis::e23 as _]
+            - velocity_in_local[Basis::e01 as _] * velocity_in_local[Basis::e12 as _],
+        velocity_in_local[Basis::e01 as _] * velocity_in_local[Basis::e31 as _]
+            - velocity_in_local[Basis::e02 as _] * velocity_in_local[Basis::e23 as _],
+        0.0,
+        0.0,
+        0.0,
+        0.0,
+        0.0,
+        0.0,
+        0.0,
+        0.0,
+    );
     DState {
-        d_world_from_local: state
-            .world_from_local
-            .geometric_product(state.velocity_in_local)
-            .scale(-0.5),
-        d_velocity_in_local: forques(state)
-            + ppga3d::Scalar::new(-0.5).geometric_product(
-                (state.velocity_in_local.dual() * state.velocity_in_local
-                    - state.velocity_in_local * state.velocity_in_local.dual())
-                .reversal()
-                .dual(),
-            ),
+        d_world_from_local: -0.5 * world_from_local * velocity_in_local,
+        d_velocity_in_local: d_velocity_in_local,
     }
 }
 
-fn forques(state: &State) -> ppga3d::Line {
-    let gravity_vector = ppga3d::Line::new(0.0, 0.0, -9.81, 0.0, 0.0, 0.0);
-    let gravity = state
-        .world_from_local
-        .reversal()
-        .transformation(gravity_vector)
-        .dual();
-    let k = -12.0;
-    let hooke = state
-        .world_from_local
-        .reversal()
-        .transformation(ATTACH_ANCHOR)
-        .regressive_product(ATTACH_IN_BODY)
-        .scale(k);
-    let damping = state.velocity_in_local.scale(-0.5);
+fn forques(state: &State) -> PGA3D {
+    let world_from_local = &state.world_from_local;
+    let velocity_in_local = &state.velocity_in_local;
+    let gravity_vector = PGA3D::e02() * -9.81;
+    let gravity = ((world_from_local.Reverse()) * gravity_vector.Dual() * world_from_local).Dual();
+    let k = 12.0;
+    let hooke =
+        k * (((world_from_local.Reverse()) * ATTACH_ANCHOR * world_from_local) & ATTACH_IN_BODY);
+    let damping = -0.25 * velocity_in_local;
     gravity // + damping
             // gravity + hooke + damping
 }
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let recording = rr::RecordingStreamBuilder::new("GA physics ppga3d")
-        .connect(rerun::default_server_addr())?;
+    let recording =
+        rr::RecordingStreamBuilder::new("GA physics ganja").connect(rerun::default_server_addr())?;
     let stable_time = rr::Timeline::new("stable_time", rr::TimeType::Time);
     rr::MsgSender::new("world")
         .with_timeless(true)
@@ -113,14 +114,14 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     let radius = rr::Radius(0.025);
 
     let points_in_local = [
-        ppga3d::Point::new(1.0, -0.5, -0.5, -0.5),
-        ppga3d::Point::new(1.0, -0.5, -0.5, 0.5),
-        ppga3d::Point::new(1.0, -0.5, 0.5, -0.5),
-        ppga3d::Point::new(1.0, -0.5, 0.5, 0.5),
-        ppga3d::Point::new(1.0, 0.5, -0.5, -0.5),
-        ppga3d::Point::new(1.0, 0.5, -0.5, 0.5),
-        ppga3d::Point::new(1.0, 0.5, 0.5, -0.5),
-        ppga3d::Point::new(1.0, 0.5, 0.5, 0.5),
+        PGA3D::point(-0.5, -0.5, -0.5),
+        PGA3D::point(-0.5, -0.5, 0.5),
+        PGA3D::point(-0.5, 0.5, -0.5),
+        PGA3D::point(-0.5, 0.5, 0.5),
+        PGA3D::point(0.5, -0.5, -0.5),
+        PGA3D::point(0.5, -0.5, 0.5),
+        PGA3D::point(0.5, 0.5, -0.5),
+        PGA3D::point(0.5, 0.5, 0.5),
     ];
 
     let edges: Vec<Edge> = (0..points_in_local.len())
@@ -135,29 +136,21 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         .collect();
 
     let mut state = State {
-        world_from_local: ppga3d::Motor::one(),
-        velocity_in_local: ppga3d::Line::new(
-            0.0, // e01
-            0.0, // e02
-            0.0, // e03
-            1.0, // e23
-            1.3, // -e13
-            0.5, // e12
-        ),
+        world_from_local: PGA3D::new(1.0, 0),
+        velocity_in_local: PGA3D::e12() + 1.3 * PGA3D::e31() + 0.5 * PGA3D::e23(),
     };
 
     let dt = 0.001;
     for i in 0..3000 {
         let d_state = dState(&state);
-        state.world_from_local += d_state.d_world_from_local.scale(dt);
-        state.velocity_in_local += d_state.d_velocity_in_local.scale(dt);
+        state.world_from_local = &state.world_from_local + &d_state.d_world_from_local * dt;
+        state.velocity_in_local = &state.velocity_in_local + &d_state.d_velocity_in_local * dt;
+        // state.world_from_local.normalize_motor_in_place();
 
         let points = points_in_local
             .iter()
             .map(|p_in_local| {
-                state
-                    .world_from_local
-                    .transformation(*p_in_local)
+                (state.world_from_local.clone() * p_in_local * state.world_from_local.Reverse())
                     .into_rerun()
             })
             .collect::<Vec<rr::Point3D>>();
@@ -174,9 +167,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
             })
             .collect::<Vec<rr::Arrow3D>>();
         let anchor = ATTACH_IN_BODY.into_rerun();
-        let point = state
-            .world_from_local
-            .transformation(ATTACH_IN_BODY)
+        let point = (&state.world_from_local * ATTACH_IN_BODY * state.world_from_local.Reverse())
             .into_rerun();
         let springs = vec![rr::Arrow3D {
             origin: [anchor.x, anchor.y, anchor.z].into(),
