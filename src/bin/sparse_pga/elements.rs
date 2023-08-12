a6d078b07ae2d0cc92cb9b331e0ef22f4732e647
Rasmus Brönnegård <1162652+rasmusgo@users.noreply.github.com>
2023-08-13 01:31:38 +0200

    Add a couple of tests in elements.rs

--- src/bin/sparse_pga/elements.rs
+++ src/bin/sparse_pga/elements.rs
@@ -23,8 +23,8 @@ pub type Line = MultiVector<
     Zero,
 >;
 
-pub type UnitMotor = MultiVector<
-    One,
+pub type Motor = MultiVector<
+    Float,
     Zero,
     Zero,
     Zero,
@@ -39,7 +39,7 @@ pub type UnitMotor = MultiVector<
     Zero,
     Zero,
     Zero,
-    Zero,
+    Float,
 >;
 
 pub type Point = MultiVector<
@@ -81,8 +81,9 @@ pub type UnitPoint = MultiVector<
 >;
 
 #[test]
-fn test_add_multivector() {
-    let motor = UnitMotor {
+fn test_add_motor_line() {
+    let motor = Motor {
+        one: 1.0,
         e01: 0.0,
         e02: 0.0,
         e03: 0.0,
@@ -100,5 +101,54 @@ fn test_add_multivector() {
         e23: 0.0,
         ..Default::default()
     };
-    let result: UnitMotor = motor + line;
+    let result: Motor = motor + line;
+}
+
+#[test]
+fn test_mul_motor_motor() {
+    let motor1 = Motor {
+        one: 1.0,
+        e01: 0.0,
+        e02: 0.0,
+        e03: 0.0,
+        e12: 0.0,
+        e31: 0.0,
+        e23: 0.0,
+        ..Default::default()
+    };
+    let motor2 = Motor {
+        one: 1.0,
+        e01: 0.0,
+        e02: 0.0,
+        e03: 0.0,
+        e12: 0.0,
+        e31: 0.0,
+        e23: 0.0,
+        ..Default::default()
+    };
+    let result: Motor = motor1 * motor2;
+}
+
+#[test]
+fn test_mul_motor_line() {
+    let motor = Motor {
+        one: 1.0,
+        e01: 0.0,
+        e02: 0.0,
+        e03: 0.0,
+        e12: 0.0,
+        e31: 0.0,
+        e23: 0.0,
+        ..Default::default()
+    };
+    let line = Line {
+        e01: 0.0,
+        e02: 0.0,
+        e03: 0.0,
+        e12: 0.0,
+        e31: 0.0,
+        e23: 0.0,
+        ..Default::default()
+    };
+    let result: Motor = motor * line;
 }
