91898b2d258f6cc0f31b4079dfbcc4b76a2bfb3c
Rasmus Brönnegård <1162652+rasmusgo@users.noreply.github.com>
2023-08-12 16:00:20 +0200

    Implement sub, mul and neg for coefficients

--- src/bin/sparse_pga/coefficient.rs
+++ src/bin/sparse_pga/coefficient.rs
@@ -1,7 +1,7 @@
 #![allow(clippy::too_many_arguments)]
 #![allow(non_camel_case_types)]
 
-use std::ops::{Add, BitAnd, BitOr, BitXor, Index, IndexMut, Mul, Not, Sub};
+use std::ops::{Add, BitAnd, BitOr, BitXor, Index, IndexMut, Mul, Neg, Not, Sub};
 
 #[derive(Default, Debug)]
 pub struct Const<const N: i32> {}
@@ -93,6 +93,92 @@ where
     }
 }
 
+// 0 - x = x
+impl<T> Sub<T> for Zero
+where
+    T: Neg,
+{
+    type Output = <T as Neg>::Output;
+
+    #[inline]
+    fn sub(self, rhs: T) -> Self::Output {
+        -rhs
+    }
+}
+
+// x - 0 = x
+impl<const N: i32> Sub<Zero> for Const<N>
+where
+    Assert<{ N != 0 }>: IsTrue,
+{
+    type Output = Const<N>;
+
+    #[inline]
+    fn sub(self, _rhs: Zero) -> Const<N> {
+        self
+    }
+}
+impl Sub<Zero> for Float {
+    type Output = Float;
+
+    #[inline]
+    fn sub(self, _rhs: Zero) -> Float {
+        self
+    }
+}
+
+// A - B
+impl<const A: i32, const B: i32> Sub<Const<B>> for Const<A>
+where
+    Assert<{ A != 0 }>: IsTrue,
+    Assert<{ B != 0 }>: IsTrue,
+    AssertNumeric<{ A - B }>: IsTrue,
+{
+    type Output = Const<{ A - B }>;
+
+    #[inline]
+    fn sub(self, _rhs: Const<B>) -> Self::Output {
+        Self::Output {}
+    }
+}
+
+// N - x
+impl<const N: i32> Sub<Float> for Const<N>
+where
+    Assert<{ N != 0 }>: IsTrue,
+{
+    type Output = Float;
+
+    #[inline]
+    fn sub(self, rhs: Float) -> Float {
+        N as Float - rhs
+    }
+}
+
+// x - N
+impl<const N: i32> Sub<Const<N>> for Float
+where
+    Assert<{ N != 0 }>: IsTrue,
+{
+    type Output = Float;
+
+    #[inline]
+    fn sub(self, _rhs: Const<N>) -> Float {
+        self - N as Float
+    }
+}
+
+impl<const N: i32> Neg for Const<N>
+where
+    AssertNumeric<{ -N }>: IsTrue,
+{
+    type Output = Const<{ -N }>;
+
+    fn neg(self) -> Self::Output {
+        Self::Output {}
+    }
+}
+
 // A == B
 impl<const A: i32, const B: i32> PartialEq<Const<B>> for Const<A> {
     #[inline]
@@ -113,6 +199,52 @@ impl<const N: i32> PartialEq<Const<N>> for Float {
     }
 }
 
+// 0 * x = 0
+impl<T> Mul<T> for Zero {
+    type Output = Zero;
+
+    #[inline]
+    fn mul(self, _rhs: T) -> Zero {
+        self
+    }
+}
+
+// x * 0 = 0
+impl<const N: i32> Mul<Zero> for Const<N>
+where
+    Assert<{ N != 0 }>: IsTrue,
+{
+    type Output = Zero;
+
+    #[inline]
+    fn mul(self, rhs: Zero) -> Zero {
+        rhs
+    }
+}
+impl Mul<Zero> for Float {
+    type Output = Zero;
+
+    #[inline]
+    fn mul(self, rhs: Zero) -> Zero {
+        rhs
+    }
+}
+
+// A * B
+impl<const A: i32, const B: i32> Mul<Const<B>> for Const<A>
+where
+    Assert<{ A != 0 }>: IsTrue,
+    Assert<{ B != 0 }>: IsTrue,
+    AssertNumeric<{ A * B }>: IsTrue,
+{
+    type Output = Const<{ A * B }>;
+
+    #[inline]
+    fn mul(self, _rhs: Const<B>) -> Self::Output {
+        Self::Output {}
+    }
+}
+
 #[test]
 fn test_add() {
     let _foo: Zero = Zero {} + Zero {};
@@ -122,10 +254,32 @@ fn test_add() {
     let _foo: One = Zero {} + One {};
     let _foo: MinusOne = MinusOne {} + Zero {};
     let _foo: MinusOne = Zero {} + MinusOne {};
+    let _foo: Const<-2> = MinusOne {} + MinusOne {};
+    let _foo: Const<2> = One {} + One {};
+}
 
-    let foo = MinusOne {} + MinusOne {};
-    assert_eq!(foo, -2.0);
+#[test]
+fn test_sub() {
+    let _foo: Zero = Zero {} - Zero {};
+    let _foo: Const<2> = One {} - MinusOne {};
+    let _foo: Const<-2> = MinusOne {} - One {};
+    let _foo: One = One {} - Zero {};
+    let _foo: MinusOne = Zero {} - One {};
+    let _foo: MinusOne = MinusOne {} - Zero {};
+    let _foo: One = Zero {} - MinusOne {};
+    let _foo: Zero = MinusOne {} - MinusOne {};
+    let _foo: Zero = One {} - One {};
+}
 
-    let foo = One {} + One {};
-    assert_eq!(foo, 2.0);
+#[test]
+fn test_mul() {
+    let _foo: Zero = Zero {} * Zero {};
+    let _foo: MinusOne = One {} * MinusOne {};
+    let _foo: MinusOne = MinusOne {} * One {};
+    let _foo: Zero = One {} * Zero {};
+    let _foo: Zero = Zero {} * One {};
+    let _foo: Zero = MinusOne {} * Zero {};
+    let _foo: Zero = Zero {} * MinusOne {};
+    let _foo: One = MinusOne {} * MinusOne {};
+    let _foo: One = One {} * One {};
 }
